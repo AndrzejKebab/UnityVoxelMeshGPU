c669657a7bf2e781a553a5b38d9c65592dd9b66f
Andrzej Kebab <grzesiugora123@wp.pl>
2024-08-11 21:46:49 +0200

    Vertex Docs improved

--- Assets/Scripts/Data/Vertex.cs
+++ Assets/Scripts/Data/Vertex.cs
@@ -6,8 +6,8 @@ namespace Data
     [StructLayout(LayoutKind.Sequential)]
     public struct Vertex
     {
-        public Vector3 Position; // 12
-        public Vector2 UV; // 8
-        public Vector3 Normal; // 12
+        public Vector3 Position; // 12 bytes
+        public Vector2 UV; // 8 bytes, for Tex2D array use Vector3 and use Z as texture index
+        public Vector3 Normal; // 12 bytes
     }
 }
