fdd00abc929ef25be0a54dc3fccfb1ec9a9f74a0
Andrzej Kebab <grzesiugora123@wp.pl>
2025-09-08 00:53:34 +0200

    Remove unused Shaders

--- Assets/Shaders/GetVertexUV.hlsl
+++
@@ -1,6 +0,0 @@
-﻿#include <Assets/Shaders/RenderPrimitivesIndirectGraphNode.hlsl>
-
-void GetVertexUV_float(in float vertexId, out float2 Out)
-{
-    Out = Vertices[Indices[vertexId]].texcoord;
-}
--- Assets/Shaders/InstanceData.hlsl
+++
@@ -1,6 +0,0 @@
-﻿StructuredBuffer<float4x4> _Matrices;
-
-void InstanceData_float(uint instanceID, out float4x4 localToWorld)
-{
-    localToWorld = _Matrices[instanceID];
-}
